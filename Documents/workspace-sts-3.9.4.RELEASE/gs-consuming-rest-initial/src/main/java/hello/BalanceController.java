e0f67464620d617d25cccf81a09df01acf44a112
palomaguenes <palomaguenes@gmail.com>
2018-06-27 18:46:24 +0300

    defining method as Get

--- Documents/workspace-sts-3.9.4.RELEASE/gs-consuming-rest-initial/src/main/java/hello/BalanceController.java
+++ Documents/workspace-sts-3.9.4.RELEASE/gs-consuming-rest-initial/src/main/java/hello/BalanceController.java
@@ -1,12 +1,13 @@
 package hello;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
 public class BalanceController {
 	
-	@RequestMapping("/balance")
+	@RequestMapping(value = {"/balance"}, method = RequestMethod.GET)
 	public Balance balance(@RequestParam(value="id") long id, @RequestParam(value="username") String name) {
 		return new Balance(id, name);
 	}
